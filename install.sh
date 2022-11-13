cec5c516dd7f4e20ec56c6a83cf296410a6687c1
Phuong Nguyen <phuongnd08@gmail.com>
2022-11-13 15:16:27 +0700

    Use install.sh to call install2.rb

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Oneliner installation"
+
+pkg update && pkg install ruby
+wget -qO- https://raw.githubusercontent.com/passive-english/passive-english/main/install2.rb | ruby
