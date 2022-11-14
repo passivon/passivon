dbc1f3098725d23b3dcb5fc93ae666f954343f29
Phuong Nguyen <phuongnd08@gmail.com>
2022-11-14 11:37:35 +0700

    Adding auto update to broadcast-daily

--- spec/broadcast_daily_spec.rb
+++ spec/broadcast_daily_spec.rb
@@ -4,7 +4,7 @@ load File.expand_path("../broadcast-daily", __dir__)
 RSpec.describe "broadcast-daily" do
   describe "in_the_right_moment?" do
     it "returns correctly" do
-      player = Player.new
+      player = MasterPlayer.new
       allow(Time).to receive(:now).and_return(double(hour: 6, min: 15))
       expect(player.in_the_right_moment?).to be_falsey
       allow(Time).to receive(:now).and_return(double(hour: 6, min: 45))
@@ -19,17 +19,23 @@ RSpec.describe "broadcast-daily" do
   describe "reload management" do
     it "works correctly" do
       now = Time.now
-      player1 = Player.new
+      player1 = MasterPlayer.new
       allow(Time).to receive(:now).and_return(now + 100)
-      killer = Player.new
+      killer = MasterPlayer.new
       allow(Time).to receive(:now).and_return(now + 200)
-      player2 = Player.new
+      player2 = MasterPlayer.new
 
       killer.kill_all
       expect(player1.should_I_die?).to be_truthy
       expect(player2.should_I_die?).to be_falsey
 
       FileUtils.rm_rf(killer.should_die_at_signal_filename)
+    end
+  end
+
+  describe "AutoUpdater" do
+    it "runs" do
+      AutoUpdater.new.perform_in_loop
     end
   end
 end
